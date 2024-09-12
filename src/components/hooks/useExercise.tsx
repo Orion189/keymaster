5a023dbe89b6316ee8870b6fb0a0b43c2cd3e8a2
Yevhen Lepekha <orion189@gmail.com>
2024-09-12 18:04:30 +0300

    Fix bugs

--- src/components/hooks/useExercise.tsx
+++ src/components/hooks/useExercise.tsx
@@ -1,25 +1,34 @@
 import { useEffect, useCallback } from 'react';
 import { letters, numbers, capitalLetters, symbols } from '@/configs/lessons.config';
-import { reaction } from 'mobx';
+import { reaction, autorun } from 'mobx';
 import { LOCALE } from '@/enums';
 import type { ExerciseGeneralType } from '@/types';
 import store from '@/store';
 import { arrShuffle } from '@/utils/common.util';
 
-type ExerciseParams = {
-    lang: LOCALE;
+type Exercise = {
     curExNum: number;
     exercises: ExerciseGeneralType;
+};
+
+type ExerciseParams = {
+    lang: LOCALE;
     isNumbersEnabled: boolean;
     isUpperCaseEnabled: boolean;
     isPunctuationEnabled: boolean;
 };
 
-const STRING_LENGTH = 80;
+const STRING_LENGTH = 60;
 
 const useExercise = () => {
     const getExercise = useCallback(
-        (lang: LOCALE, curExNum: number, isNumbersEnabled: boolean, isUpperCaseEnabled: boolean, isPunctuationEnabled: boolean) => {
+        (
+            lang: LOCALE,
+            curExNum: number,
+            isNumbersEnabled: boolean,
+            isUpperCaseEnabled: boolean,
+            isPunctuationEnabled: boolean
+        ) => {
             let chars = [];
             const lettersConfig = letters[lang][curExNum];
             const capitalLettersConfig = capitalLetters[lang][curExNum];
@@ -55,51 +64,90 @@ const useExercise = () => {
         },
         []
     );
-    const setExercise = useCallback((params: ExerciseParams, prevParams: ExerciseParams) => {
-        const { lang, exercises, curExNum, isNumbersEnabled, isUpperCaseEnabled, isPunctuationEnabled } = params;
-        const isTogglesChanged =
+    const initExercise = useCallback(
+        (params: Exercise & ExerciseParams) => {
+            const { lang, exercises, curExNum, isNumbersEnabled, isUpperCaseEnabled, isPunctuationEnabled } = params;
+            const isExersiseEmpty =
+                exercises[lang].length === 0 ||
+                !exercises[lang][curExNum] ||
+                exercises[lang][curExNum].chars.length === 0;
+
+            if (!lang || !letters[lang]) {
+                return;
+            }
+
+            if (isExersiseEmpty) {
+                const exercise = getExercise(
+                    lang,
+                    curExNum,
+                    isNumbersEnabled,
+                    isUpperCaseEnabled,
+                    isPunctuationEnabled
+                );
+
+                if (exercise) {
+                    const newExercises = exercises[lang].slice();
+
+                    newExercises[curExNum] = exercise;
+
+                    store.set('app', {
+                        ...store.app,
+                        exercises: {
+                            ...exercises,
+                            [lang]: newExercises
+                        }
+                    });
+                }
+            }
+        },
+        [getExercise]
+    );
+    const handleExerciseParamsChange = useCallback((params: ExerciseParams, prevParams: ExerciseParams) => {
+        const { lang, isNumbersEnabled, isUpperCaseEnabled, isPunctuationEnabled } = params;
+        const isParamsChanged =
+            lang !== prevParams.lang ||
             isNumbersEnabled !== prevParams.isNumbersEnabled ||
             isUpperCaseEnabled !== prevParams.isUpperCaseEnabled ||
             isPunctuationEnabled !== prevParams.isPunctuationEnabled;
 
-        if (!letters[lang] || !lang) {
+        if (!lang || !letters[lang]) {
             return;
         }
 
-        if (!Array.isArray(exercises[lang])) {
-            exercises[lang] = [];
+        if (isParamsChanged) {
+            store.set('app', {
+                ...store.app,
+                exercises: {
+                    [LOCALE.EN]: [],
+                    [LOCALE.UK]: [],
+                    [LOCALE.RU]: []
+                }
+            });
         }
-
-        if (!exercises[lang][curExNum] || isTogglesChanged) {
-            const exercise = getExercise(
-                lang,
-                curExNum,
-                isNumbersEnabled,
-                isUpperCaseEnabled,
-                isPunctuationEnabled
-            );
-
-            if (exercise) {
-                exercises[lang][curExNum] = exercise;
-    
-                store.set('app', { ...store.app, exercises });
-            }
-        }
-    }, [getExercise]);
+    }, []);
 
     useEffect(() => {
-        reaction(
-            () => ({
+        autorun(() => {
+            initExercise({
                 lang: store.settings.lang,
                 curExNum: store.app.curExNum,
                 exercises: store.app.exercises,
                 isNumbersEnabled: store.settings.isNumbersEnabled,
                 isUpperCaseEnabled: store.settings.isUpperCaseEnabled,
                 isPunctuationEnabled: store.settings.isPunctuationEnabled
+            });
+        });
+
+        reaction(
+            () => ({
+                lang: store.settings.lang,
+                isNumbersEnabled: store.settings.isNumbersEnabled,
+                isUpperCaseEnabled: store.settings.isUpperCaseEnabled,
+                isPunctuationEnabled: store.settings.isPunctuationEnabled
             }),
-            (params, prevParams) => setExercise(params, prevParams)
+            (params, prevParams) => handleExerciseParamsChange(params, prevParams)
         );
-    }, [setExercise]);
+    }, [initExercise, handleExerciseParamsChange]);
 
     return null;
 };
