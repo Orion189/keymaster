b992587559c793df4f2f3d9f1c53b8c20cf4ac2e
Yevhen Lepekha <orion189@gmail.com>
2024-09-29 16:51:00 +0300

    Fix Windows styles

--- src/components/main/BottomBlock/index.tsx
+++ src/components/main/BottomBlock/index.tsx
@@ -1,11 +1,12 @@
 import { memo, FC } from 'react';
 import MacKeyboard from '@components/main/MacKeyboard';
 import WinKeyboard from '@components/main/WinKeyboard';
+import styles from './style.module.scss';
 
 const BottomBlock: FC = memo(() => {
     const isMac = window.__APP_PLATFORM__ === 'darwin';
 
-    return isMac ? <MacKeyboard /> : <WinKeyboard />;
+    return <div className={styles.cont}>{isMac ? <MacKeyboard /> : <WinKeyboard />}</div>;
 });
 
 export default BottomBlock;
